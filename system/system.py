b6bfc776f2c70a97e4ac27cc8644e60aad9628f0
mattwthompson <mattwthompson@protonmail.com>
2020-05-15 17:21:06 -0500

    Make barebones system class

--- system/system.py
+++ system/system.py
@@ -1,34 +1,11 @@
-"""
-system.py
-A molecular system object from the Open Force Field Initiative
-
-Handles the primary functions
-"""
+from pydantic import BaseModel
 
 
-def canvas(with_attribution=True):
-    """
-    Placeholder function to show example docstring (NumPy format)
+class System(BaseModel):
+    """The OpenFF System object."""
 
-    Replace this function and doc string for your own project
+    def to_file(self):
+        raise NotImplementedError()
 
-    Parameters
-    ----------
-    with_attribution : bool, Optional, default: True
-        Set whether or not to display who the quote is from
-
-    Returns
-    -------
-    quote : str
-        Compiled string including quote and optional attribution
-    """
-
-    quote = "The code is but a canvas to our imagination."
-    if with_attribution:
-        quote += "\n\t- Adapted from Henry David Thoreau"
-    return quote
-
-
-if __name__ == "__main__":
-    # Do something if this file is invoked on its own
-    print(canvas())
+    def from_file(self):
+        raise NotImplementedError()
