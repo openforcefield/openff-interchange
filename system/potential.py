e9acbb6458dfe6d212c59de73b5d14e7233a7fe6
mattwthompson <mattwthompson@protonmail.com>
2020-05-20 20:04:20 -0500

    Make separate classes for parametrized and un-parametrized potentials

--- system/potential.py
+++ system/potential.py
@@ -1,4 +1,4 @@
-from typing import Union, Dict, Set
+from typing import Dict, Set, Union
 
 from pint import Quantity
 from pydantic import BaseModel, validator
@@ -6,10 +6,13 @@ from sympy import Expr
 
 
 class AnalyticalPotential(BaseModel):
-    """Generic representation of an interaction potential."""
+    """
+    Generic representation of an interaction potential having an analytic
+    form and lacking parameters.
+    """
+
     name: str
     expression: Union[Expr, str]
-    parameters: Dict[Union[Expr, str], Quantity]
     independent_variables: Set[Union[Expr, str]]
 
     @validator("expression")
@@ -21,3 +24,13 @@ class AnalyticalPotential(BaseModel):
 
     class Config:
         arbitrary_types_allowed = True
+
+
+class ParametrizedAnalyticalPotential(AnalyticalPotential):
+    """AnalyticalPotential but filled with parameters."""
+
+    parameters: Dict[Union[Expr, str], Quantity]
+
+    @validator("parameters")
+    def is_valid(cls, val, values):
+        return val
--- system/tests/test_potential.py
+++ system/tests/test_potential.py
@@ -1,29 +1,42 @@
 from sympy import Expr
-from pint import Quantity
+import pint
 
-from system.potential import AnalyticalPotential
+from system.potential import AnalyticalPotential, ParametrizedAnalyticalPotential
 from system.tests.base_test import BaseTest
 
+
+u = pint.UnitRegistry()
 
 class TestPotential(BaseTest):
     def test_analytical_potential_constructor(self):
         pot = AnalyticalPotential(
-            name="X",
-            expression=Expr("x+1"),
-            independent_variables={Expr("j")},
-            parameters={Expr("x"): Quantity(1)},
+            name="TestPotential",
+            expression=Expr("mx+b"),
+            independent_variables={Expr("x")},
         )
 
-        assert pot.name == "X"
-        assert pot.expression == Expr("x+1")
-        assert pot.independent_variables == {Expr("j")}
-        assert pot.parameters == dict({Expr("x"): Quantity(1)})
+        assert pot.name == "TestPotential"
+        assert pot.expression == Expr("mx+b")
 
         pot_from_str = AnalyticalPotential(
-            name="X",
-            expression="x+1",
-            independent_variables={Expr("j")},
-            parameters={Expr("x"): Quantity(1)},
+            name="TestPotentialFromString",
+            expression="mx+b",
+            independent_variables={Expr("x")},
         )
 
         assert pot.expression == pot_from_str.expression
+
+    def test_parametrized_analytical_potential_constructor(self):
+        pot = ParametrizedAnalyticalPotential(
+            name="TestPotential",
+            expression=Expr("mx+b"),
+            independent_variables={Expr("x")},
+            parameters={"a": 0.5 * u.dimensionless, "b": -1.0 * u.dimensionless},
+        )
+
+        assert pot.name == "TestPotential"
+        assert pot.expression == Expr("mx+b")
+        assert "a" in pot.parameters.keys()
+        assert "b" in pot.parameters.keys()
+        assert pot.parameters["a"] == 0.5 * u.dimensionless
+        assert pot.parameters["b"] == -1.0 * u.dimensionless
