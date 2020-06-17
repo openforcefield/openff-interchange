c81121b82133a4e2ec794d5baa01a288b8a7918b
Matthew W. Thompson <matt.thompson@vanderbilt.edu>
2020-06-17 13:13:35 -0500

    Update SMIRNOFFHandlerNotImplementedError message

--- system/exceptions.py
+++ system/exceptions.py
@@ -1,9 +1,10 @@
 class SMIRNOFFHandlerNotImplementedError(Exception):
-    def __init__(self, name=None):
-        self.name = name
+    def __init__(self, args):
+        if args:
+            self.name = args[0]
 
     def __str__(self):
-        msg = f"SMIRNOFF parameter not implemented here. "
+        msg = "SMIRNOFF parameter not implemented here. "
         if self.name:
-            msg += "Tried to parse ParameterHandler of name {self.name}"
+            msg += f"Tried to parse ParameterHandler of name {self.name}"
         return msg
