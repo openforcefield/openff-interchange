97d604188a55d7e328b99b3d6ea4cbd3ed3573d3
Matthew W. Thompson <matt.thompson@vanderbilt.edu>
2020-06-29 18:05:28 -0500

    MAINT: Initial namespace migration

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ openff-system
 A molecular system object from the Open Force Field Initiative
 """
 import sys
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 import versioneer
 
 short_description = __doc__.split("\n")
@@ -34,7 +34,7 @@ setup(
     # Which Python importable modules should be included when your package is installed
     # Handled automatically by setuptools. Use 'exclude' to prevent some specific
     # subpackage(s) from being added, if needed
-    packages=find_packages(),
+    packages=find_namespace_packages(),
 
     # Optional include package data to ship with your package
     # Customize MANIFEST.in if the general case does not suit your needs
