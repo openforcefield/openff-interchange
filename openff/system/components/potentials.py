3a105fe908015c8d7ea2181abd81cce9382b054c
Matthew W. Thompson <mattwthompson@protonmail.com>
2020-10-15 16:47:31 -0500

    ENH: Begin refactor to concise potential handlers

---
+++ openff/system/components/potentials.py
@@ -0,0 +1,37 @@
+from typing import Dict, Set, Union
+
+from pydantic import BaseModel, validator
+from sympy import Expr
+
+from openff.system import unit
+
+
+class Potential(BaseModel):
+    """Base class for storing applied parameters"""
+
+    required_parameters: Dict[str, unit.Quantity] = dict()
+
+    class Config:
+        arbitrary_types_allowed = True
+        validate_assignment = True
+
+
+class PotentialHandler(BaseModel):
+    """Base class for storing parametrized force field data"""
+
+    name: str
+    expression: Union[Expr, str]
+    independent_variables: Union[str, Set[Union[Expr, str]]]
+    slot_map: Dict[tuple, str] = dict()
+    potentials: Dict[str, str] = dict()
+
+    @validator("expression")
+    def is_valid_sympy_expr(cls, val):
+        if isinstance(val, Expr):
+            return str(val)
+        elif isinstance(val, str):
+            return val
+
+    class Config:
+        arbitrary_types_allowed = True
+        validate_assignment = True
