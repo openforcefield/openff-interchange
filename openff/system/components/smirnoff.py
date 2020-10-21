d6f7faff9daefead1da63a0f5df988894ea04875
Matthew W. Thompson <mattwthompson@protonmail.com>
2020-10-21 17:34:13 -0500

    ENH: Change SMIRNOFFBondHandler to only take in corresponding ParameterHandler

--- openff/system/components/smirnoff.py
+++ openff/system/components/smirnoff.py
@@ -1,7 +1,7 @@
 from typing import Dict, Set
 
 from openforcefield.topology.topology import Topology
-from openforcefield.typing.engines.smirnoff.forcefield import ForceField
+from openforcefield.typing.engines.smirnoff.parameters import BondHandler
 
 from openff.system.components.potentials import Potential, PotentialHandler
 from openff.system.utils import simtk_to_pint
@@ -15,17 +15,17 @@ class SMIRNOFFBondHandler(PotentialHandler):
     slot_map: Dict[tuple, str] = dict()
     potentials: Dict[str, Potential] = dict()
 
-    def store_matches(self, forcefield: ForceField, topology: Topology) -> None:
+    def store_matches(self, parameter_handler: BondHandler, topology: Topology) -> None:
         """
         Populate self.slot_map with key-val pairs of slots
         and unique potential identifiers
 
         """
-        matches = forcefield["Bonds"].find_matches(topology)
+        matches = parameter_handler.find_matches(topology)
         for key, val in matches.items():
             self.slot_map[key] = val.parameter_type.smirks
 
-    def store_potentials(self, forcefield: ForceField) -> None:
+    def store_potentials(self, parameter_handler: BondHandler) -> None:
         """
         Populate self.potentials with key-val pairs of unique potential
         identifiers and their associated Potential objects
@@ -34,7 +34,7 @@ class SMIRNOFFBondHandler(PotentialHandler):
         for smirks in self.slot_map.values():
             # ParameterHandler.get_parameter returns a list, although this
             # should only ever be length 1
-            parameter_type = forcefield["Bonds"].get_parameter({"smirks": smirks})[0]
+            parameter_type = parameter_handler.get_parameter({"smirks": smirks})[0]
             potential = Potential(
                 parameters={
                     "k": simtk_to_pint(parameter_type.k),
