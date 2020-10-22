74eb37534cac64e5aaf56a3d34149b94a0253b40
Matthew W. Thompson <mattwthompson@protonmail.com>
2020-10-21 20:17:53 -0500

    ENH: Add SMIRNOFFAngleHandler

--- openff/system/components/smirnoff.py
+++ openff/system/components/smirnoff.py
@@ -1,7 +1,7 @@
 from typing import Dict, Set
 
 from openforcefield.topology.topology import Topology
-from openforcefield.typing.engines.smirnoff.parameters import BondHandler
+from openforcefield.typing.engines.smirnoff.parameters import AngleHandler, BondHandler
 
 from openff.system.components.potentials import Potential, PotentialHandler
 from openff.system.utils import simtk_to_pint
@@ -39,6 +39,45 @@ class SMIRNOFFBondHandler(PotentialHandler):
                 parameters={
                     "k": simtk_to_pint(parameter_type.k),
                     "length": simtk_to_pint(parameter_type.length),
+                },
+            )
+            self.potentials[smirks] = potential
+
+
+class SMIRNOFFAngleHandler(PotentialHandler):
+
+    name: str = "Angles"
+    expression: str = "1/2 * k * (angle - theta)"
+    independent_variables: Set[str] = {"theta"}
+    slot_map: Dict[tuple, str] = dict()
+    potentials: Dict[str, Potential] = dict()
+
+    def store_matches(
+        self, parameter_handler: AngleHandler, topology: Topology
+    ) -> None:
+        """
+        Populate self.slot_map with key-val pairs of slots
+        and unique potential identifiers
+
+        """
+        matches = parameter_handler.find_matches(topology)
+        for key, val in matches.items():
+            self.slot_map[key] = val.parameter_type.smirks
+
+    def store_potentials(self, parameter_handler: AngleHandler) -> None:
+        """
+        Populate self.potentials with key-val pairs of unique potential
+        identifiers and their associated Potential objects
+
+        """
+        for smirks in self.slot_map.values():
+            # ParameterHandler.get_parameter returns a list, although this
+            # should only ever be length 1
+            parameter_type = parameter_handler.get_parameter({"smirks": smirks})[0]
+            potential = Potential(
+                parameters={
+                    "k": simtk_to_pint(parameter_type.k),
+                    "angle": simtk_to_pint(parameter_type.angle),
                 },
             )
             self.potentials[smirks] = potential
--- openff/system/stubs.py
+++ openff/system/stubs.py
@@ -3,9 +3,9 @@ Monkeypatching external classes with custom functionality
 """
 from openforcefield.topology.topology import Topology
 from openforcefield.typing.engines.smirnoff import ForceField
-from openforcefield.typing.engines.smirnoff.parameters import BondHandler
+from openforcefield.typing.engines.smirnoff.parameters import AngleHandler, BondHandler
 
-from openff.system.components.smirnoff import SMIRNOFFBondHandler
+from openff.system.components.smirnoff import SMIRNOFFAngleHandler, SMIRNOFFBondHandler
 
 
 class System:
@@ -25,13 +25,17 @@ def to_openff_system(self, topology: Topology, **kwargs) -> System:
     system.handlers = dict()
 
     bonds = self["Bonds"].create_bond_potential_handler(topology=topology, **kwargs)
+    angles = self["Angles"].create_angle_potential_handler(topology=topology, **kwargs)
     system.handlers.update({"Bonds": bonds})
+    system.handlers.update({"Angles": angles})
 
     return system
 
 
 def create_bond_potential_handler(
-    self, topology: Topology, **kwargs
+    self,
+    topology: Topology,
+    **kwargs,
 ) -> SMIRNOFFBondHandler:
     """
     A method, patched onto BondHandler, that creates a corresponding SMIRNOFFBondHandler
@@ -44,5 +48,22 @@ def create_bond_potential_handler(
     return handler
 
 
+def create_angle_potential_handler(
+    self,
+    topology: Topology,
+    **kwargs,
+) -> SMIRNOFFAngleHandler:
+    """
+    A method, patched onto BondHandler, that creates a corresponding SMIRNOFFBondHandler
+
+    """
+    handler = SMIRNOFFAngleHandler()
+    handler.store_matches(parameter_handler=self, topology=topology)
+    handler.store_potentials(parameter_handler=self)
+
+    return handler
+
+
 BondHandler.create_bond_potential_handler = create_bond_potential_handler
+AngleHandler.create_angle_potential_handler = create_angle_potential_handler
 ForceField.create_openff_system = to_openff_system
