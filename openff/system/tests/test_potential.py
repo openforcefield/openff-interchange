58364b30346a0f00f534ea7f654a8f528846e6d8
Matthew W. Thompson <mattwthompson@protonmail.com>
2020-11-03 16:56:56 -0600

    TST: Add some PotentialHandler tests

--- openff/system/tests/test_potential.py
+++ openff/system/tests/test_potential.py
@@ -1,48 +1,54 @@
-from sympy import Expr
+from openforcefield.topology import Molecule, Topology
+from openforcefield.typing.engines.smirnoff.parameters import AngleHandler, BondHandler
+from simtk import unit as omm_unit
 
-from openff.system import unit
-from openff.system.components.potentials import Potential, PotentialHandler
-
-from ..tests.base_test import BaseTest
+from openff.system.tests.base_test import BaseTest
+from openff.system.utils import simtk_to_pint
 
 
-class TestPotentialHandler(BaseTest):
-    """Test the behavior of PotentialHandler objects"""
+class TestBondPotentialHandler(BaseTest):
+    def test_bond_parameter_handler(self):
+        top = Topology.from_molecules(Molecule.from_smiles("O=O"))
 
-    def test_potential_handler_constructor(self):
-        """Test a basic construction"""
-        PotentialHandler(name="foo", expression="m*x+b", independent_variables="x")
-
-    def test_sympy_expr(self):
-        """Test that sympy Expr are parsed and stored as str"""
-        as_str = PotentialHandler(
-            name="foo", expression="m*x+b", independent_variables="x"
+        bond_handler = BondHandler(version=0.3)
+        bond_parameter = BondHandler.BondType(
+            smirks="[*:1]~[*:2]",
+            k=1.5 * omm_unit.kilocalorie_per_mole / omm_unit.angstrom ** 2,
+            length=1.5 * omm_unit.angstrom,
+            id="b1000",
         )
-        as_expr = PotentialHandler(
-            name="foo", expression=Expr("m*x+b"), independent_variables="x"
+        bond_handler.add_parameter(bond_parameter.to_dict())
+
+        from openff.system.stubs import ForceField
+
+        forcefield = ForceField()
+        forcefield.register_parameter_handler(bond_handler)
+        bond_potentials = forcefield["Bonds"].create_potential(top)
+
+        pot = bond_potentials.potentials[bond_potentials.slot_map[(0, 1)]]
+        kcal_ang2_mol = omm_unit.kilocalorie_per_mole / omm_unit.angstrom ** 2
+
+        assert pot.parameters["k"] == simtk_to_pint(1.5 * kcal_ang2_mol)
+
+    def test_angle_parameter_handler(self):
+        top = Topology.from_molecules(Molecule.from_smiles("CCC"))
+
+        angle_handler = AngleHandler(version=0.3)
+        angle_parameter = AngleHandler.AngleType(
+            smirks="[*:1]~[*:2]~[*:3]",
+            k=2.5 * omm_unit.kilocalorie_per_mole / omm_unit.degree ** 2,
+            angle=100 * omm_unit.degree,
+            id="b1000",
         )
+        angle_handler.add_parameter(angle_parameter.to_dict())
 
-        assert as_str.dict() == as_expr.dict()
+        from openff.system.stubs import ForceField
 
+        forcefield = ForceField()
+        forcefield.register_parameter_handler(angle_handler)
+        angle_potentials = forcefield["Angles"].create_potential(top)
 
-class TestPotential(BaseTest):
-    """Test the behavior of Potential objects"""
+        pot = angle_potentials.potentials[angle_potentials.slot_map[(0, 1, 2)]]
+        kcal_deg2_mol = omm_unit.kilocalorie_per_mole / omm_unit.degree ** 2
 
-    def test_potential_constructor(self):
-        """Test the constructor of a single Potential in a PotentialHandler"""
-        handler = PotentialHandler(
-            name="foo", expression="m*x+b", independent_variables="x"
-        )
-        handler.slot_map.update({(0,): "line1"})
-        handler.potentials.update(
-            {
-                "line1": Potential(
-                    parameters={"m": 1 * unit.meter, "b": -1 * unit.second}
-                )
-            }
-        )
-
-        param = handler.potentials[handler.slot_map[(0,)]]
-
-        assert param.parameters["m"] == 1 * unit.meter
-        assert param.parameters["b"] == -1 * unit.second
+        assert pot.parameters["k"] == simtk_to_pint(2.5 * kcal_deg2_mol)
