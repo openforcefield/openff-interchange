4df08cd4e6204eeaf2949741ffbabbab54eacf6f
Matthew W. Thompson <mattwthompson@protonmail.com>
2020-12-16 08:17:44 -0600

    TST: Test OpenMM export on multi-molecule systems

--- openff/system/tests/test_interop/test_openmm.py
+++ openff/system/tests/test_interop/test_openmm.py
@@ -6,8 +6,8 @@ from simtk import openmm, unit
 from openff.system.stubs import ForceField
 
 
-@pytest.mark.parametrize("mol", ["C", "CC"])
-def test_from_openmm(mol):
+@pytest.mark.parametrize("mol,n_mols", [("C", 1), ("CC", 1), ("C", 2), ("CC", 2)])
+def test_from_openmm_single_mols(mol, n_mols):
     """
     Test that ForceField.create_openmm_system and System.to_openmm produce
     objects with similar energies
@@ -20,19 +20,26 @@ def test_from_openmm(mol):
 
     mol = Molecule.from_smiles(mol)
     mol.generate_conformers(n_conformers=1)
-    top = Topology.from_molecules([mol])
+    top = Topology.from_molecules(n_mols * [mol])
     top.box_vectors = np.asarray([4, 4, 4]) * unit.nanometer
+
+    if n_mols == 1:
+        positions = mol.conformers[0]
+    elif n_mols == 2:
+        positions = np.vstack(
+            [mol.conformers[0], mol.conformers[0] + 2 * unit.nanometer]
+        )
 
     toolkit_energy = _get_energy_from_openmm_system(
         openmm_sys=parsley.create_openmm_system(top),
         openmm_top=top.to_openmm(),
-        positions=mol.conformers[0],
+        positions=positions,
     )
 
     system_energy = _get_energy_from_openmm_system(
         openmm_sys=parsley.create_openff_system(top).to_openmm(),
         openmm_top=top.to_openmm(),
-        positions=mol.conformers[0],
+        positions=positions,
     )
 
     np.testing.assert_allclose(
