BUG: Fix consistency of position units in tests

openff/system/tests/test_interop/test_internal_writers.py
@@ -1,16 +1,19 @@
 import numpy as np
 from openforcefield.topology import Molecule
+from simtk import unit as omm_unit
 
 from openff.system.stubs import ForceField
 
 
 def test_internal_gro_writer():
-    top = Molecule.from_smiles("C").to_topology()
+    mol = Molecule.from_smiles("C")
+    mol.generate_conformers(n_conformers=1)
+    top = mol.to_topology()
     parsley = ForceField("openff-1.0.0.offxml")
     out = parsley.create_openff_system(top)
 
     out.box = [4, 4, 4] * np.eye(3)
-    out.positions = np.random.rand(15).reshape((5, 3))
+    out.positions = mol.conformers[0] / omm_unit.nanometer
 
     out.to_gro("internal.gro", writer="internal")
     out.to_gro("parmed.gro", writer="parmed")