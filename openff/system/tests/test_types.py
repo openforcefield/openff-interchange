6b7bf794784a00e203a582ea23de81428ed4a5c6
Matthew W. Thompson <mattwthompson@protonmail.com>
2021-01-07 13:48:52 -0600

    MAINT: Remove old arry model and tests

--- openff/system/tests/test_types.py
+++ openff/system/tests/test_types.py
@@ -2,40 +2,9 @@ import json
 
 import numpy as np
 import pytest
-from pydantic import BaseModel
 
 from openff.system import unit
-from openff.system.types import ArrayQuantity, DefaultModel, FloatQuantity, UnitArray
-
-
-class UnitModel(BaseModel):
-
-    any_values: UnitArray
-    distance_values: UnitArray[unit.nm]
-    time_values: UnitArray[unit.ns]
-
-
-class TestUnitArray:
-    def test_pint_model(self):
-
-        model = UnitModel(
-            any_values=[4, 2, 1] * unit.year,
-            distance_values=[1, 2] * unit.nm,
-            time_values=[2.0, 4.0] * unit.ns,
-        )
-
-        assert model.any_values.units == unit.year
-        assert model.distance_values.units == unit.nm
-        assert model.time_values.units == unit.ns
-
-    @pytest.mark.parametrize("default_unit", [unit.second, unit.liter, unit.meter])
-    def test_default_units(self, default_unit):
-        assert UnitArray([1, 1, 1], units=default_unit).units == default_unit
-
-    @pytest.mark.parametrize("input", [0, int, type(None)])
-    def test_bad_inputs(self, input):
-        with pytest.raises(TypeError):
-            UnitArray([4, 4], units=input)
+from openff.system.types import ArrayQuantity, DefaultModel, FloatQuantity
 
 
 class TestQuantityTypes:
--- openff/system/types.py
+++ openff/system/types.py
@@ -145,33 +145,3 @@ class DefaultModel(BaseModel):
         }
         json_loads = json_loader
         validate_assignment = True
-
-
-class UnitArrayMeta(type):
-    # TODO: would be nice to be able to sneak dtype and/or units in here
-    def __getitem__(self, units, dtype=None):
-        return type("UnitArray", (UnitArray,), {"_dtype": dtype, "_units": units})
-
-
-class UnitArray(unit.Quantity, metaclass=UnitArrayMeta):
-    """
-    Thin wrapper around pint.Quantity for compliance with Pydantic classes
-
-    See https://github.com/samuelcolvin/pydantic/issues/380#issuecomment-594639970
-    """
-
-    # TODO: Handle various cases of implicit units, i.e. NumPy arrays that intend
-    # TODO: Use dtype
-
-    @classmethod
-    def __get_validators__(cls):
-        yield cls.validate_type
-
-    @classmethod
-    def validate_type(cls, val):
-        try:
-            val = unit.Quantity(val)
-            return val
-        # TODO: Handle other exceptions, like pint.UndefinedUnitError
-        except TypeError as e:
-            raise TypeError from e
