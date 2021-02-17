91e4862ef08d3acf744ed4e50bc899ff21afa292
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2021-02-16 19:05:23 -0600

    ENH: Add foyer Atom and Bond Handlers

--- openff/system/components/foyer.py
+++ openff/system/components/foyer.py
@@ -1,9 +1,55 @@
-from typing import Dict, Set
+from copy import copy
+from typing import Dict, Set, Any, Tuple
+
 
 import parmed as pmd
+
 from foyer import Forcefield
 
+from openff.toolkit.topology import Topology
+from openff.system import unit as u
+from openff.system.components.system import System
 from openff.system.components.potentials import Potential, PotentialHandler
+from openff.system.exceptions import MissingParametersError
+
+
+def _copy_params(params: Dict[str, float],
+                 *drop_keys: str,
+                 param_units: Dict = None) -> Dict:
+    params_copy = copy(params)
+    for drop_key in drop_keys:
+        params_copy.pop(drop_key, None)
+    if param_units:
+        for unit_item, units in param_units.items():
+            params_copy[unit_item] = params_copy[unit_item] * units
+    return params_copy
+
+
+def _get_openmm_force_gen(ff: Forcefield, gen_type: type) -> Any:
+    return list(
+            filter(
+                lambda x: isinstance(x, gen_type),
+                ff.getGenerators()
+            )
+        ).pop()
+
+
+def _topology_from_parmed(structure: pmd.Structure) -> Topology:
+    """ToDo: Create a openFF Topology from a parmed structure"""
+
+
+def from_foyer(structure: pmd.Structure,
+               ff: Forcefield,
+               **kwargs) -> System:
+    """Create an openFF system object from a parmed structure by applying a foyer Forcefield"""
+    system = System()
+    system.topology = _topology_from_parmed(structure)
+    foyer_atom_handler = FoyerAtomTypes()
+    foyer_bond_handler = FoyerBondHandler()
+    system.handlers['FoyerAtomHandler'] = foyer_atom_handler
+    system.handlers['FoyerBondHandler'] = foyer_bond_handler
+
+    return system
 
 
 class FoyerAtomTypes(PotentialHandler):
@@ -25,12 +71,38 @@ class FoyerAtomTypes(PotentialHandler):
 
         """
         type_map = forcefield.run_atomtyping(structure)
-
         for key, val in type_map.items():
             self.slot_map[key] = val["atomtype"]
 
-    def store_potentials(self):
-        """"""
+    def store_potentials(self, forcefield) -> None:
+        from simtk.openmm.app.forcefield import NonbondedGenerator
+        non_bonded_forces_gen = _get_openmm_force_gen(forcefield, NonbondedGenerator)
+
+        if non_bonded_forces_gen:
+            nonbonded_params = non_bonded_forces_gen.params.paramsForType
+
+            for atom_idx in self.slot_map:
+                try:
+                    params = _copy_params(
+                        nonbonded_params[self.slot_map[atom_idx]],
+                        'charge',
+                        param_units={
+                            'epsilon': u.kcal / u.mol,
+                            'sigma': u.nm
+                        })
+                except KeyError:
+                    raise MissingParametersError(
+                        f'Missing parameters for atomtype {self.slot_map[atom_idx]}'
+                        'in the forcefield'
+                    )
+
+                self.potentials[atom_idx] = Potential(
+                    parameters=params
+                )
+        else:
+            raise MissingParametersError(
+                'The forcefield is missing NonBondedForces Parameters'
+            )
 
 
 class FoyerBondHandler(PotentialHandler):
@@ -38,17 +110,51 @@ class FoyerBondHandler(PotentialHandler):
     name: str = "Bonds"
     expression: str = "1/2 * k * (r - length) ** 2"
     independent_variables: Set[str] = {"r"}
-    slot_map: Dict[str, str] = dict()
-    potentials: Dict[str, Potential] = dict()
+    slot_map: Dict[Tuple[int, int], Tuple[str, str]] = dict()
+    potentials: Dict[Tuple[int, int], Potential] = dict()
 
-    def make_slot_map(self):
-        pass
-        # populates a dict with the structure like
-        # self.slot_map = {"(0, 1)": ["opls_140", "opls_141"]}
+    def store_matches(self,
+                      structure: pmd.Structure,
+                      atom_slots: Dict[str, str]) -> None:
+        for bond in structure.bonds:
+            atom_1_idx = bond.atom1.idx
+            atom_2_idx = bond.atom2.idx
 
-    def lookup_key(self):
-        for identifier in self.slot_map.values():
-            # look up FF data from identifier (or directly consult the
-            # topology, which would involve droppint the make_slot_map method)
-            # store FF data into a Potential object
-            self.potentials[identifier] = Potential(...)
+            self.slot_map[(atom_1_idx, atom_2_idx)] = \
+                (atom_slots[atom_1_idx], atom_slots[atom_2_idx])
+
+    def store_potentials(self, forcefield: Forcefield) -> None:
+        from simtk.openmm.app.forcefield import HarmonicBondGenerator
+        harmonic_bond_forces_gen = _get_openmm_force_gen(forcefield, HarmonicBondGenerator)
+        bonds_for_atom_types = harmonic_bond_forces_gen.bondsForAtomType
+
+        for (atom_1_idx, atom_2_idx), (atom_1_type, atom_2_type) in self.slot_map.items():
+            for i in bonds_for_atom_types[atom_1_type]:
+                types1 = harmonic_bond_forces_gen.types1[i]
+                types2 = harmonic_bond_forces_gen.types2[i]
+
+                # Replicated from
+                # https://github.com/openmm/openmm/blob/b49b82efb5a253a7c891ca084b3370e181de2ea3/wrappers/python/simtk/openmm/app/forcefield.py#L1960-L1983
+
+                if (atom_1_type in types1 and atom_2_type in types2) \
+                        or (atom_1_type in types2 and atom_2_type in types1):
+                    params = {
+                        'k': harmonic_bond_forces_gen.k[i] * u.kcal / u.mol / u.nm**2,
+                        'length': harmonic_bond_forces_gen.length[i] * u.nm
+                    }
+
+                    self.potentials[(atom_1_idx, atom_2_idx)] = Potential(parameters=params)
+                    break
+
+            if not self.potentials.get((atom_1_idx, atom_2_idx)):
+                raise MissingParametersError(
+                    f'Could not find parameters for the Bond between '
+                    f'atoms {atom_1_type}-{atom_2_type}'
+                )
+
+
+        # for identifier in self.slot_map.values():
+        #     # look up FF data from identifier (or directly consult the
+        #     # topology, which would involve droppint the make_slot_map method)
+        #     # store FF data into a Potential object
+        #     self.potentials[identifier] = Potential(...)
---
+++ openff/system/tests/test_foyer.py
@@ -0,0 +1,26 @@
+import pytest
+import parmed as pmd
+
+from openff.system.components.foyer import from_foyer
+from openff.system.tests.base_test import BaseTest
+
+
+class TestFoyer(BaseTest):
+
+    @pytest.fixture(scope='session')
+    def oplsaa_system_ethane(self):
+        import foyer
+        from foyer.tests.utils import get_fn
+        oplsaa = foyer.Forcefield(name='oplsaa')
+        pmd_ethane = pmd.load_file(get_fn('ethane.mol2'), structure=True)
+        system = from_foyer(
+            structure=pmd_ethane,
+            ff=oplsaa
+        )
+        return system
+
+    def test_from_foyer_system_atom_handlers(self, oplsaa_system_ethane):
+        assert oplsaa_system_ethane.handlers.get('FoyerAtomHandler')
+
+    def test_from_foyer_system_bond_handlers(self, oplsaa_system_ethane):
+        assert oplsaa_system_ethane.handlers.get('FoyerBondHandler')
