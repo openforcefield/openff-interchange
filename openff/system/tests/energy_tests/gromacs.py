c42ad0d26eda0bcc3515fb74ab351aece8c25401
Matthew W. Thompson <mattwthompson@protonmail.com>
2021-04-02 12:15:07 -0500

    DOC: Add initial documentation, docstrings to energy tests

--- openff/system/tests/energy_tests/gromacs.py
+++ openff/system/tests/energy_tests/gromacs.py
@@ -13,7 +13,7 @@ from openff.system.tests.energy_tests.report import EnergyReport
 from openff.system.utils import get_test_file_path
 
 
-def get_mdp_file(key: str) -> Path:
+def _get_mdp_file(key: str) -> Path:
     mapping = {
         "default": "default.mdp",
         "cutoff": "cutoff.mdp",
@@ -30,27 +30,74 @@ def get_gromacs_energies(
     writer: str = "internal",
     electrostatics=True,
 ) -> EnergyReport:
+    """
+    Given an OpenFF System object, return single-point energies as computed by GROMACS.
+
+    .. warning :: This API is experimental and subject to change.
+
+    Parameters
+    ----------
+    off_sys : openff.system.components.system.System
+        An OpenFF System object to compute the single-point energy of
+    mdp : str, default="cutoff"
+        A string key identifying the GROMACS `.mdp` file to be used. See `_get_mdp_file`.
+    writer : str, default="internal"
+        A string key identifying the backend to be used to write GROMACS files. The
+        default value of `"internal"` results in this package's exporters being used.
+    electrostatics : bool, default=True
+        A boolean indicating whether or not electrostatics should be included in the energy
+        calculation.
+
+    Returns
+    -------
+    report : EnergyReport
+        An `EnergyReport` object containing the single-point energies.
+
+    """
     with tempfile.TemporaryDirectory() as tmpdir:
         with temporary_cd(tmpdir):
             off_sys.to_gro("out.gro", writer=writer)
             off_sys.to_top("out.top", writer=writer)
-            return run_gmx_energy(
+            report = _run_gmx_energy(
                 top_file="out.top",
                 gro_file="out.gro",
-                mdp_file=get_mdp_file(mdp),
+                mdp_file=_get_mdp_file(mdp),
                 maxwarn=2,
                 electrostatics=electrostatics,
             )
+            return report
 
 
-def run_gmx_energy(
+def _run_gmx_energy(
     top_file: Union[Path, str],
     gro_file: Union[Path, str],
     mdp_file: Union[Path, str],
     maxwarn: int = 1,
     electrostatics=True,
 ):
+    """
+    Given GROMACS files, return single-point energies as computed by GROMACS.
 
+    Parameters
+    ----------
+    top_file : str or pathlib.Path
+        The path to a GROMACS topology (`.top`) file.
+    gro_file : str or pathlib.Path
+        The path to a GROMACS coordinate (`.gro`) file.
+    mdp_file : str or pathlib.Path
+        The path to a GROMACS molecular dynamics parameters (`.mdp`) file.
+    maxwarn : int, default=1
+        The number of warnings to allow when `gmx grompp` is called (via the `-maxwarn` flag).
+    electrostatics : bool, default=True
+        A boolean indicated whether or not electrostatics should be included in the energy
+        calculation.
+
+    Returns
+    -------
+    report : EnergyReport
+        An `EnergyReport` object containing the single-point energies.
+
+    """
     grompp_cmd = f"gmx grompp --maxwarn {maxwarn} -o out.tpr"
     grompp_cmd += f" -f {mdp_file} -c {gro_file} -p {top_file}"
 
@@ -99,11 +146,13 @@ def run_gmx_energy(
     if energy.returncode:
         raise GMXRunError(err)
 
-    return _parse_gmx_energy("out.xvg", electrostatics=electrostatics)
+    report = _parse_gmx_energy("out.xvg", electrostatics=electrostatics)
+
+    return report
 
 
 def _get_gmx_energy_nonbonded(gmx_energies: Dict):
-    """Get the total nonbonded energy from a set of GROMACS energies"""
+    """Get the total nonbonded energy from a set of GROMACS energies."""
     gmx_nonbonded = 0.0 * omm_unit.kilojoule_per_mole
     for key in ["LJ (SR)", "Coulomb (SR)", "Coul. recip.", "Disper. corr."]:
         try:
@@ -115,6 +164,7 @@ def _get_gmx_energy_nonbonded(gmx_energies: Dict):
 
 
 def _parse_gmx_energy(xvg_path, electrostatics=True):
+    """Parse an `.xvg` file written by `gmx energy`."""
     energies, _ = _group_energy_terms(xvg_path)
 
     # TODO: Better way of filling in missing fields
--- openff/system/tests/test_parmed.py
+++ openff/system/tests/test_parmed.py
@@ -4,9 +4,9 @@ from simtk import unit as omm_unit
 from openff.system.interop.parmed import from_parmed
 from openff.system.tests.base_test import BaseTest
 from openff.system.tests.energy_tests.gromacs import (
+    _get_mdp_file,
+    _run_gmx_energy,
     get_gromacs_energies,
-    get_mdp_file,
-    run_gmx_energy,
 )
 from openff.system.utils import get_test_file_path
 
@@ -24,17 +24,17 @@ class TestParmEd(BaseTest):
         roundtrip.save("conv.gro", overwrite=True)
         roundtrip.save("conv.top", overwrite=True)
 
-        original_energy = run_gmx_energy(
+        original_energy = _run_gmx_energy(
             top_file=get_test_file_path("ALA_GLY/ALA_GLY.top"),
             gro_file=get_test_file_path("ALA_GLY/ALA_GLY.gro"),
-            mdp_file=get_mdp_file("cutoff_hbonds"),
+            mdp_file=_get_mdp_file("cutoff_hbonds"),
         )
         internal_energy = get_gromacs_energies(openff_sys, mdp="cutoff_hbonds")
 
-        roundtrip_energy = run_gmx_energy(
+        roundtrip_energy = _run_gmx_energy(
             top_file="conv.top",
             gro_file="conv.gro",
-            mdp_file=get_mdp_file("cutoff_hbonds"),
+            mdp_file=_get_mdp_file("cutoff_hbonds"),
         )
 
         # Differences in bond energies appear to be related to ParmEd's rounding
