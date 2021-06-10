f4757288f8de5ac0816f79637cfa4200e3797220
Matthew W. Thompson <mattwthompson@protonmail.com>
2021-06-10 12:35:02 -0500

    REF: OpenFF System -> OpenFF Interchange

---
+++ openff/interchange/tests/interop/test_external.py
@@ -0,0 +1,103 @@
+import mdtraj as md
+import numpy as np
+import pytest
+from openff.toolkit.topology import Molecule
+from openff.toolkit.utils import get_data_file_path
+from openff.units import unit
+from simtk import openmm
+from simtk.unit import nanometer as nm
+
+from openff.interchange.components.mdtraj import OFFBioTop
+from openff.interchange.drivers import get_openmm_energies
+from openff.interchange.drivers.openmm import _get_openmm_energies
+from openff.interchange.stubs import ForceField
+from openff.interchange.tests import BaseTest
+from openff.interchange.utils import get_test_file_path
+
+
+class TestFromOpenMM(BaseTest):
+    @pytest.mark.slow
+    def test_from_openmm_pdbfile(self, argon_ff, argon_top):
+        pdb_file_path = get_test_file_path("10-argons.pdb")
+        pdbfile = openmm.app.PDBFile(pdb_file_path)
+
+        mol = Molecule.from_smiles("[#18]")
+        top = OFFBioTop.from_openmm(pdbfile.topology, unique_molecules=[mol])
+        top.mdtop = md.Topology.from_openmm(top.to_openmm())
+        box = pdbfile.topology.getPeriodicBoxVectors()
+        box = box.value_in_unit(nm) * unit.nanometer
+
+        out = argon_ff.create_openff_interchange(top)
+        out.box = box
+        out.positions = pdbfile.getPositions()
+
+        assert np.allclose(
+            out.positions.to(unit.nanometer).magnitude,
+            pdbfile.getPositions().value_in_unit(nm),
+        )
+
+        get_openmm_energies(out, hard_cutoff=True).compare(
+            _get_openmm_energies(
+                omm_sys=argon_ff.create_openmm_system(top),
+                box_vectors=pdbfile.topology.getPeriodicBoxVectors(),
+                positions=pdbfile.getPositions(),
+                hard_cutoff=True,
+            )
+        )
+
+    @pytest.fixture
+    def unique_molecules(self):
+        molecules = ["O", "C1CCCCC1", "C", "CCC", "CCO", "CCCCO"]
+        return [Molecule.from_smiles(mol) for mol in molecules]
+        # What if, instead ...
+        # Molecule.from_iupac(molecules)
+
+    @pytest.mark.slow
+    @pytest.mark.parametrize(
+        "pdb_path",
+        [
+            ("cyclohexane_ethanol_0.4_0.6.pdb"),
+            ("cyclohexane_water.pdb"),
+            ("ethanol_water.pdb"),
+            ("propane_methane_butanol_0.2_0.3_0.5.pdb"),
+        ],
+    )
+    def test_from_toolkit_packmol_boxes(self, pdb_path, unique_molecules):
+        """
+        Test loading some pre-prepared PACKMOL-generated systems.
+
+        These use PDB files already prepared in the toolkit because PDB files are a pain.
+        """
+        ff = ForceField("openff-1.0.0.offxml")
+
+        pdb_file_path = get_data_file_path("systems/packmol_boxes/" + pdb_path)
+        pdbfile = openmm.app.PDBFile(pdb_file_path)
+        top = OFFBioTop.from_openmm(
+            pdbfile.topology,
+            unique_molecules=unique_molecules,
+        )
+        top.mdtop = md.Topology.from_openmm(top.to_openmm())
+        box = pdbfile.topology.getPeriodicBoxVectors()
+        box = box.value_in_unit(nm) * unit.nanometer
+
+        out = ff.create_openff_interchange(top)
+        out.box = box
+        out.positions = pdbfile.getPositions()
+
+        assert np.allclose(
+            out.positions.to(unit.nanometer).magnitude,
+            pdbfile.getPositions().value_in_unit(nm),
+        )
+
+        get_openmm_energies(
+            out,
+            hard_cutoff=True,
+            combine_nonbonded_forces=True,
+        ).compare(
+            _get_openmm_energies(
+                omm_sys=ff.create_openmm_system(top),
+                box_vectors=pdbfile.topology.getPeriodicBoxVectors(),
+                positions=pdbfile.getPositions(),
+                hard_cutoff=True,
+            )
+        )
---
+++ openff/interchange/tests/test_utils.py
@@ -0,0 +1,66 @@
+import numpy as np
+from openff.toolkit.typing.engines.smirnoff import ForceField
+from openff.units import unit
+from openff.units.simtk import from_simtk
+from simtk import unit as simtk_unit
+
+from openff.interchange.tests import BaseTest
+from openff.interchange.utils import (
+    compare_forcefields,
+    get_partial_charges_from_openmm_system,
+    pint_to_simtk,
+    unwrap_list_of_pint_quantities,
+)
+
+simtk_quantitites = [
+    4.0 * simtk_unit.nanometer,
+    5.0 * simtk_unit.angstrom,
+    1.0 * simtk_unit.elementary_charge,
+]
+
+pint_quantities = [
+    4.0 * unit.nanometer,
+    5.0 * unit.angstrom,
+    1.0 * unit.elementary_charge,
+]
+
+
+def test_simtk_list_of_quantities_to_pint():
+    """Test conversion from Quantity lists, lists of Quantity"""
+    list_of_quantities = [val * simtk_unit.meter for val in range(10)]
+    quantity_list = simtk_unit.meter * [val for val in range(10)]
+
+    assert list_of_quantities != quantity_list
+    assert all(from_simtk(list_of_quantities) == from_simtk(quantity_list))
+
+
+def test_pint_to_simtk():
+    """Test conversion from pint Quantity to SimTK Quantity."""
+    q = 5.0 / unit.nanometer
+    assert pint_to_simtk(q) == 0.5 / simtk_unit.angstrom
+
+
+class TestUtils(BaseTest):
+    def test_compare_forcefields(self, parsley):
+        parsley_name = "openff-1.0.0.offxml"
+        compare_forcefields(parsley, parsley)
+        compare_forcefields(ForceField(parsley_name), parsley)
+        compare_forcefields(parsley, ForceField(parsley_name))
+        compare_forcefields(ForceField(parsley_name), ForceField(parsley_name))
+
+    def test_unwrap_quantities(self):
+        wrapped = [1 * unit.m, 1.5 * unit.m]
+        unwrapped = [1, 1.5] * unit.m
+
+        assert all(unwrapped == unwrap_list_of_pint_quantities(wrapped))
+
+
+class TestOpenMM(BaseTest):
+    def test_openmm_partial_charges(self, argon_ff, argon_top):
+        omm_system = argon_ff.create_openmm_system(argon_top)
+        partial_charges = get_partial_charges_from_openmm_system(omm_system)
+
+        # assert isinstance(partial_charges, unit.Quantity)
+        # assert partial_charges.units == unit.elementary_charge
+        assert isinstance(partial_charges, list)
+        assert np.allclose(partial_charges, np.zeros(4))  # .magnitude
