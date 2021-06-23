2cc5f574c7f26b513a35213994167a3ef071cee8
Matthew W. Thompson <mattwthompson@protonmail.com>
2021-06-23 11:12:06 -0500

    DOC: Document TopologyKey and PotentialKey

--- openff/interchange/models.py
+++ openff/interchange/models.py
@@ -7,6 +7,8 @@ from openff.interchange.types import custom_quantity_encoder, json_loader
 
 
 class DefaultModel(BaseModel):
+    """A custom Pydantic model used by other components."""
+
     class Config:
         json_encoders = {
             unit.Quantity: custom_quantity_encoder,
@@ -17,6 +19,39 @@ class DefaultModel(BaseModel):
 
 
 class TopologyKey(DefaultModel):
+    """A unique identifier of a segment of a chemical topology.
+
+    These refer to a single portion of a chemical graph, i.e. a single valence term,
+    (a bond, angle, or dihedral) or a single atom. These target only the information in
+    the chemical graph and do not store physics parameters. For example, a TopologyKey
+    corresponding to a bond would store the indices of the two atoms that compose the
+    bond, but not the force constant or equilibrium bond length as determined by the
+    force field.
+
+    Examples
+    --------
+
+    Create a TopologyKey identifying some speicfic angle
+
+    >>> from openff.interchange.models import TopologyKey
+    >>> this_angle = TopologyKey(atom_indices=(2, 1, 3))
+    >>> this_angle
+    TopologyKey(atom_indices=(2, 1, 3), mult=None)
+
+    Create a TopologyKey indentifying just one atom
+
+    >>> this_atom = TopologyKey(atom_indices=(4,))
+    >>> this_atom
+    TopologyKey(atom_indices=(4,), mult=None)
+
+    Layer multiple TopologyKey objects that point to the same torsion
+
+    >>> key1 = TopologyKey(atom_indices=(1, 2, 5, 6), mult=0)
+    >>> key2 = TopologyKey(atom_indices=(1, 2, 5, 6), mult=1)
+    >>> assert key1 != key2
+
+    """
+
     atom_indices: Tuple[int, ...] = Field(
         tuple(), description="The indices of the atoms occupied by this interaction"
     )
@@ -29,6 +64,36 @@ class TopologyKey(DefaultModel):
 
 
 class PotentialKey(DefaultModel):
+    """A unique identifier of a segment of a chemical topology.
+
+    These refer to a single term in a force field as applied to a single segment of a chemical
+    topology, i.e. a single atom or dihedral. For example, a PotentialKey corresponding to a
+    bond would store the the force constant and the equilibrium bond length as determined by
+    the force field. These keys to not have direct knowledge of where in a topology they have been
+    applied.
+
+    Examples
+    --------
+
+    Create a PotentialKey corresponding to the parameter with id `b55` in OpenFF "Parsley" 1.0.0
+
+    >>> from openff.interchange.models import PotentialKey
+    >>> from openff.toolkit.typing.engines.smirnoff import ForceField
+    >>> parsley = ForceField("openff-1.0.0.offxml")
+    >>> param = parsley['Bonds'].get_parameter({"id": 'b55'})[0]
+    >>> bond_55 = PotentialKey(id=param.smirks)
+    >>> bond_55
+    PotentialKey(id='[#16X4,#16X3:1]-[#8X2:2]', mult=None, associated_handler=None)
+
+    Create a PotentialKey corresponding to the angle parameters in OPLS-AA defined
+    between atom types opls_135, opls_135, and opls_140
+
+    >>> oplsaa_angle = PotentialKey(id='opls_135-opls_135-opls_140')
+    >>> oplsaa_angle
+    PotentialKey(id='opls_135-opls_135-opls_140', mult=None, associated_handler=None)
+
+    """
+
     id: str = Field(
         ...,
         description="A unique identifier of this potential, i.e. a SMARTS pattern or an atom type",
