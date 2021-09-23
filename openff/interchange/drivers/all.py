7e296d3cefb2a7767a2c225b7d9783ef2b26efa1
Matthew W. Thompson <mattwthompson@protonmail.com>
2021-09-23 11:01:17 -0500

    ENH: Add get_summary_data

--- openff/interchange/drivers/all.py
+++ openff/interchange/drivers/all.py
@@ -1,6 +1,8 @@
 """Functions for running energy evluations with all available engines."""
 from typing import TYPE_CHECKING, Dict
 
+from openff.utilities.utilities import requires_package
+
 from openff.interchange.drivers.amber import get_amber_energies
 from openff.interchange.drivers.gromacs import get_gromacs_energies
 from openff.interchange.drivers.lammps import get_lammps_energies
@@ -8,6 +10,8 @@ from openff.interchange.drivers.openmm import get_openmm_energies
 from openff.interchange.drivers.report import EnergyReport
 
 if TYPE_CHECKING:
+    from pandas import DataFrame
+
     from openff.interchange.components.interchange import Interchange
 
 
@@ -15,9 +19,29 @@ def get_all_energies(interchange: "Interchange") -> Dict[str, EnergyReport]:
     """
     Given an Interchange object, return single-point energies as computed by all available engines.
     """
+    # TODO: Return something nan-like if one driver fails, but still return others that succeed
+    # TODO: Have each driver return the version of the engine that was used
+
     return {
         "OpenMM": get_openmm_energies(interchange),
         "GROMACS": get_gromacs_energies(interchange),
         "LAMMPS": get_lammps_energies(interchange),
         "Amber": get_amber_energies(interchange),
     }
+
+
+@requires_package("pandas")
+def get_summary_data(interchange: "Interchange") -> "DataFrame":
+    """Return a pandas DataFrame with summaries of energies from all available engines."""
+    from openff.units import unit
+    from pandas import DataFrame
+
+    kj_mol = unit.kilojoule / unit.mol
+
+    energies = get_all_energies(interchange)
+
+    for k, v in energies.items():
+        for kk in v.energies:
+            energies[k].energies[kk] = energies[k].energies[kk].m_as(kj_mol)
+
+    return DataFrame({k: v.energies for k, v in energies.items()}).T
