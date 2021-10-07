3ed3d2af6777d9fd3534c30e595d8d7c7a0c8bc3
Matthew W. Thompson <mattwthompson@protonmail.com>
2021-10-07 10:05:06 -0500

    ENH: Add shim for combining topologies (via MDTraj)

--- openff/interchange/components/mdtraj.py
+++ openff/interchange/components/mdtraj.py
@@ -115,3 +115,56 @@ def _get_num_h_bonds(mdtop):
             n_bonds_containing_hydrogen += 1
 
     return n_bonds_containing_hydrogen
+
+
+def _combine_topologies(topology1: _OFFBioTop, topology2: _OFFBioTop) -> _OFFBioTop:
+    """
+    Experimental shim for combining _OFFBioTop objects.
+
+    Note that this really only operates on the mdtops.
+    """
+    mdtop1 = copy.deepcopy(topology1.mdtop)
+    mdtop2 = copy.deepcopy(topology2.mdtop)
+
+    mdtop = md.Topology()
+    first_topology_chain = mdtop.add_chain()
+    second_topology_chain = mdtop.add_chain()
+
+    for residue in mdtop1.residues:
+        this_residue = mdtop.add_residue(
+            name=residue.name,
+            chain=first_topology_chain,
+            resSeq=residue.resSeq,
+            segment_id=residue.segment_id,
+        )
+        for atom in residue.atoms:
+            mdtop.add_atom(atom.name, atom.element, this_residue)
+
+    for residue in mdtop2.residues:
+        this_residue = mdtop.add_residue(
+            name=residue.name,
+            chain=second_topology_chain,
+            resSeq=residue.resSeq,
+            segment_id=residue.segment_id,
+        )
+        for atom in residue.atoms:
+            mdtop.add_atom(atom.name, atom.element, this_residue)
+
+    atom_offset = mdtop1.n_atoms
+
+    for bond in mdtop1.bonds:
+        mdtop.add_bond(
+            atom1=mdtop.atom(bond.atom1.index),
+            atom2=mdtop.atom(bond.atom2.index),
+        )
+
+    for bond in mdtop2.bonds:
+        mdtop.add_bond(
+            atom1=mdtop.atom(bond.atom1.index + atom_offset),
+            atom2=mdtop.atom(bond.atom2.index + atom_offset),
+        )
+
+    combined_topology = _OFFBioTop()
+    combined_topology.mdtop = mdtop
+
+    return combined_topology
--- openff/interchange/unit_tests/components/test_mdtraj.py
+++ openff/interchange/unit_tests/components/test_mdtraj.py
@@ -6,6 +6,7 @@ from openmm import app
 
 from openff.interchange.components.interchange import Interchange
 from openff.interchange.components.mdtraj import (
+    _combine_topologies,
     _get_num_h_bonds,
     _iterate_pairs,
     _iterate_propers,
@@ -75,3 +76,19 @@ def test_get_num_h_bonds():
     top = mol.to_topology()
     mdtop = md.Topology.from_openmm(top.to_openmm())
     assert _get_num_h_bonds(mdtop) == 6
+
+
+def test_combine_topologies():
+    molecule = Molecule.from_smiles("CCO")
+    molecule.name = "ETH"
+
+    topology = molecule.to_topology()
+
+    top = _OFFBioTop()
+    top.mdtop = md.Topology.from_openmm(topology.to_openmm())
+
+    combined = _combine_topologies(top, top)
+
+    for attr in ("atoms", "bonds", "chains", "residues"):
+        attr = "n_" + attr
+        assert getattr(combined.mdtop, attr) == 2 * getattr(top.mdtop, attr)
