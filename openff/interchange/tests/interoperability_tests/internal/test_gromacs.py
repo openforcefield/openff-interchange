b2954cc1c73fdaac2e1cba225b552f7d6ecbb749
Matthew W. Thompson <mattwthompson@protonmail.com>
2022-07-07 11:28:51 -0500

    FIX: Update GROMACS tests for new virtual site functionality

--- openff/interchange/tests/interoperability_tests/internal/test_gromacs.py
+++ openff/interchange/tests/interoperability_tests/internal/test_gromacs.py
@@ -14,7 +14,6 @@ from pkg_resources import resource_filename
 from openff.interchange import Interchange
 from openff.interchange.components.nonbonded import BuckinghamvdWHandler
 from openff.interchange.components.potentials import Potential
-from openff.interchange.components.smirnoff import SMIRNOFFVirtualSiteHandler
 from openff.interchange.drivers import get_gromacs_energies, get_openmm_energies
 from openff.interchange.exceptions import GMXMdrunError, UnsupportedExportError
 from openff.interchange.interop.internal.gromacs import from_gro
@@ -298,12 +297,12 @@ class TestGROMACSVirtualSites(_BaseTest):
         """Fixture that loads an SMIRNOFF XML for argon"""
         virtual_site_handler = VirtualSiteHandler(version=0.3)
 
-        carbonyl_type = VirtualSiteHandler.VirtualSiteMonovalentLonePairType(
+        carbonyl_type = VirtualSiteHandler.VirtualSiteType(
             name="EP",
             smirks="[O:1]=[C:2]-[*:3]",
             distance=0.3 * unit.angstrom,
             type="MonovalentLonePair",
-            match="once",
+            match="all_permutations",
             outOfPlaneAngle=0.0 * unit.degree,
             inPlaneAngle=120.0 * unit.degree,
             charge_increment1=0.05 * unit.elementary_charge,
@@ -329,18 +328,6 @@ class TestGROMACSVirtualSites(_BaseTest):
         )
         out.box = [4, 4, 4]
         out.positions = mol.conformers[0]
-        out.handlers["VirtualSites"] = SMIRNOFFVirtualSiteHandler._from_toolkit(
-            parameter_handler=sage_with_sigma_hole["VirtualSites"],
-            topology=mol.to_topology(),
-        )
-        out["vdW"]._from_toolkit_virtual_sites(
-            parameter_handler=sage_with_sigma_hole["VirtualSites"],
-            topology=mol.to_topology(),
-        )
-        out["Electrostatics"]._from_toolkit_virtual_sites(
-            parameter_handler=sage_with_sigma_hole["VirtualSites"],
-            topology=mol.to_topology(),
-        )
 
         # TODO: Sanity-check reported energies
         get_gromacs_energies(out)
@@ -364,20 +351,7 @@ class TestGROMACSVirtualSites(_BaseTest):
         )
         out.box = [4, 4, 4]
         out.positions = mol.conformers[0]
-        out.handlers["VirtualSites"] = SMIRNOFFVirtualSiteHandler._from_toolkit(
-            parameter_handler=sage_with_monovalent_lone_pair["VirtualSites"],
-            topology=mol.to_topology(),
-        )
-        out["vdW"]._from_toolkit_virtual_sites(
-            parameter_handler=sage_with_monovalent_lone_pair["VirtualSites"],
-            topology=mol.to_topology(),
-        )
-        out["Electrostatics"]._from_toolkit_virtual_sites(
-            parameter_handler=sage_with_monovalent_lone_pair["VirtualSites"],
-            topology=mol.to_topology(),
-        )
 
-        # TODO: Sanity-check reported energies
-        get_gromacs_energies(out)
-
-        # ParmEd does not support 3fad, and cannot be used to sanity check charges
+        with pytest.raises(Exception, match="MonovalentLonePair not implemented."):
+            # TODO: Sanity-check reported energies
+            get_gromacs_energies(out)
