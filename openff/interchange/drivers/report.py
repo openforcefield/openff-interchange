5c79bb07f44ff109d1607ba8c60cdc7cf3c89f2b
Matthew W. Thompson <mattwthompson@protonmail.com>
2022-08-02 12:50:32 -0500

    MAINT: Fix nightly and beta testing workflows

--- openff/interchange/drivers/report.py
+++ openff/interchange/drivers/report.py
@@ -45,7 +45,7 @@ class EnergyReport(DefaultModel):
         if item in self.energies.keys():
             return self.energies[item]
         if item.lower() == "total":
-            return sum(self.energies.values())  # type: ignore[type-var]
+            return sum(self.energies.values())  # type: ignore[arg-type]
         else:
             return None
 
