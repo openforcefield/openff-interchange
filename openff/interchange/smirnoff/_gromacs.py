2323dd714497baa12a666410ae75238e6919fa5d
Matthew W. Thompson <mattwthompson@protonmail.com>
2023-04-20 09:30:36 -0500

    ENH: Add API for adding and removing GROMACS molecules

--- openff/interchange/smirnoff/_gromacs.py
+++ openff/interchange/smirnoff/_gromacs.py
@@ -157,6 +157,15 @@ def _convert(interchange: Interchange) -> GROMACSSystem:
                 ),
             )
 
+            this_molecule_atom_type_names = tuple(
+                atom.atom_type for atom in molecule.atoms
+            )
+
+            molecule._contained_atom_types = {
+                atom_type_name: system.atom_types[atom_type_name]
+                for atom_type_name in this_molecule_atom_type_names
+            }
+
         # Use a set to de-duplicate
         pairs: set[tuple] = {*_get_14_pairs(unique_molecule)}
 
