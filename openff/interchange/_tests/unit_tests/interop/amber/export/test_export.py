bedd438992f993fdb6ecd2fcb9eef1bfd7e0b308
Matthew W. Thompson <mattwthompson@protonmail.com>
2023-04-24 16:50:27 -0500

    TST: Use PDB from issue

--- openff/interchange/_tests/unit_tests/interop/amber/export/test_export.py
+++ openff/interchange/_tests/unit_tests/interop/amber/export/test_export.py
@@ -10,7 +10,7 @@ def test_atom_names_with_padding():
     # Unclear if the toolkit will always load PDBs with padded whitespace in name
     Interchange.from_smirnoff(
         ForceField("openff-2.0.0.offxml"),
-        Molecule.from_file(get_test_file_path("ethanol.pdb")).to_topology(),
+        Molecule.from_file(get_test_file_path("lig_CHEMBL3265016-1.pdb")).to_topology(),
     ).to_prmtop("tmp.prmtop")
 
     # #679 persists, but #678 would cause an earlier error
