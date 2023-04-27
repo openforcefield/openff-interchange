07f794d4f09943f99b54d5479cf0bd55a8445b11
Matthew W. Thompson <mattwthompson@protonmail.com>
2023-04-27 14:36:54 -0500

    TST: Test number of exclusions in some ring systems

--- openff/interchange/_tests/unit_tests/interop/amber/export/test_export.py
+++ openff/interchange/_tests/unit_tests/interop/amber/export/test_export.py
@@ -1,3 +1,5 @@
+import openmm
+import openmm.app
 import parmed
 import pytest
 from openff.toolkit import ForceField, Molecule
@@ -7,7 +9,6 @@ from openff.interchange import Interchange
 from openff.interchange._tests import get_test_file_path
 
 
-# pytest processes fixtures before the decorator can be applied
 @requires_openeye
 @pytest.mark.parametrize(
     "molecule",
@@ -17,6 +18,7 @@ from openff.interchange._tests import get_test_file_path
     ],
 )
 def test_atom_names_with_padding(molecule):
+    # pytest processes fixtures before the decorator can be applied
     if molecule.endswith(".pdb"):
         molecule = Molecule(get_test_file_path(molecule).as_posix())
     else:
@@ -30,3 +32,28 @@ def test_atom_names_with_padding(molecule):
 
     # Loading with ParmEd striggers #679 if exclusions lists are wrong
     parmed.load_file("tmp.prmtop")
+
+
+@pytest.mark.parametrize("molecule", ["C1=CN=CN1", "c1ccccc1", "c1ccc2ccccc2c1"])
+def exclusions_in_rings(molecule):
+    molecule.generate_conformers(n_conformers=1)
+    topology = molecule.to_topology()
+    topology.box_vectors = [4, 4, 4]
+
+    sage_no_impropers = ForceField("openff-2.0.0.offxml")
+    sage_no_impropers.deregister_parameter_handler("ImproperTorsions")
+
+    interchange = sage_no_impropers.create_interchange(topology)
+
+    interchange.to_prmtop("tmp.prmtop")
+
+    # Use the OpenMM export as source of truth
+    openmm_system = interchange.to_openmm()
+    for force in openmm_system.getForces():
+        if isinstance(force, openmm.NonbondedForce):
+            reference = force.getNumExceptions()
+
+    loaded_system = openmm.app.AmberPrmtopFile("tmp.prmtop").createSystem()
+    for force in loaded_system.getForces():
+        if isinstance(force, openmm.NonbondedForce):
+            assert force.getNumExceptions() == reference
