1abc4e778e35c4a77493b3b7a93abeb9866738b9
Matthew W. Thompson <mattwthompson@protonmail.com>
2023-08-14 17:48:52 -0500

    FIX: Begin rewriting virtual site position generation

--- openff/interchange/interop/_virtual_sites.py
+++ openff/interchange/interop/_virtual_sites.py
@@ -4,7 +4,7 @@ Common helpers for exporting virtual sites.
 from collections import defaultdict
 
 import numpy
-from openff.units import unit
+from openff.units import Quantity, unit
 
 from openff.interchange import Interchange
 from openff.interchange.exceptions import (
@@ -36,7 +36,7 @@ def _virtual_site_parent_molecule_mapping(
 def get_positions_with_virtual_sites(
     interchange: Interchange,
     use_zeros: bool = False,
-) -> unit.Quantity:
+) -> Quantity:
     """Return the positions of all particles (atoms and virtual sites)."""
     if interchange.positions is None:
         raise MissingPositionsError(
@@ -56,7 +56,7 @@ def get_positions_with_virtual_sites(
     for virtual_site, molecule_index in virtual_site_molecule_map.items():
         molecule_virtual_site_map[molecule_index].append(virtual_site)
 
-    particle_positions = unit.Quantity(
+    particle_positions = Quantity(
         numpy.empty(shape=(0, 3)),
         unit.nanometer,
     )
@@ -74,13 +74,13 @@ def get_positions_with_virtual_sites(
         )
 
         if use_zeros:
-            this_molecule_virtual_site_positions = unit.Quantity(
+            this_molecule_virtual_site_positions = Quantity(
                 numpy.zeros((n_virtual_sites_in_this_molecule, 3)),
                 unit.nanometer,
             )
 
         else:
-            this_molecule_virtual_site_positions = unit.Quantity(
+            this_molecule_virtual_site_positions = Quantity(
                 numpy.asarray(
                     [
                         _get_virtual_site_positions(virtual_site_key, interchange)
@@ -106,7 +106,7 @@ def get_positions_with_virtual_sites(
 def _get_virtual_site_positions(
     virtual_site_key: VirtualSiteKey,
     interchange: Interchange,
-) -> unit.Quantity:
+) -> Quantity:
     # TODO: Move this behavior elsewhere, possibly to a non-GROMACS location
     if virtual_site_key.type == "BondCharge":
         return _get_bond_charge_virtual_site_positions(virtual_site_key, interchange)
@@ -124,24 +124,65 @@ def _get_virtual_site_positions(
 def _get_bond_charge_virtual_site_positions(
     virtual_site_key,
     interchange,
-) -> unit.Quantity:
-    r0 = interchange.positions[virtual_site_key.orientation_atom_indices[0]]
-    r1 = interchange.positions[virtual_site_key.orientation_atom_indices[1]]
-
-    # numpy.linalg.norm(r0 - r1) requires something unimplemented with __array_function__
-    r1_r0_bond_length = numpy.sqrt(numpy.sum((r0 - r1) ** 2))
-
+) -> Quantity:
     potential_key = interchange["VirtualSites"].key_map[virtual_site_key]
     potential = interchange["VirtualSites"].potentials[potential_key]
     distance = potential.parameters["distance"]
 
-    return r1 + (r1 - r0) * (distance / r1_r0_bond_length)
+    # r1 and r2 are positions of atom1 and atom2 in the convention of
+    # these diagrams:
+    # https://docs.openforcefield.org/projects/toolkit/en/stable/users/virtualsites.html#applying-virtual-site-parameters
+    r1 = interchange.positions[virtual_site_key.orientation_atom_indices[0]]
+    r2 = interchange.positions[virtual_site_key.orientation_atom_indices[1]]
+
+    r2_r1_norm = numpy.linalg.norm((r2 - r1).m_as(unit.angstrom)) * unit.angstrom
+
+    # The virtual site is placed at a distance opposite the r1 -> r2 vector
+    return r1 - (r2 - r1) * (distance / r2_r1_norm)
+
+
+def _get_monovalent_lone_pair_virtual_site_positions(
+    virtual_site_key,
+    interchange,
+) -> Quantity:
+    # r1 and r2 are positions of atom1 and atom2 in the convention of
+    # these diagrams:
+    # https://docs.openforcefield.org/projects/toolkit/en/stable/users/virtualsites.html#applying-virtual-site-parameters
+    r1 = interchange.positions[virtual_site_key.orientation_atom_indices[0]]
+    r2 = interchange.positions[virtual_site_key.orientation_atom_indices[1]]
+
+    potential_key = interchange["VirtualSites"].key_map[virtual_site_key]
+    potential = interchange["VirtualSites"].potentials[potential_key]
+    distance = potential.parameters["distance"]
+    in_plane_angle = potential.parameters["inPlaneAngle"]
+    out_of_plane_angle = potential.parameters["outOfPlaneAngle"]
+
+    if out_of_plane_angle.m != 0.0:
+        raise NotImplementedError(
+            "Only planar `MonovalentLonePairType` is currently supported.",
+        )
+
+    if in_plane_angle.m != 0.0:
+        raise NotImplementedError(
+            "Only linear `MonovalentLonePairType` is currently supported.",
+        )
+
+    # r1 and r2 are positions of atom1 and atom2 in the convention of
+    # these diagrams:
+    # https://docs.openforcefield.org/projects/toolkit/en/stable/users/virtualsites.html#applying-virtual-site-parameters
+    r1 = interchange.positions[virtual_site_key.orientation_atom_indices[0]]
+    r2 = interchange.positions[virtual_site_key.orientation_atom_indices[1]]
+
+    r2_r1_norm = numpy.linalg.norm((r2 - r1).m_as(unit.angstrom)) * unit.angstrom
+
+    # The virtual site is placed at a distance opposite the r1 -> r2 vector
+    return r1 - (r2 - r1) * (distance / r2_r1_norm)
 
 
 def _get_divalent_lone_pair_virtual_site_positions(
     virtual_site_key,
     interchange,
-) -> unit.Quantity:
+) -> Quantity:
     r0 = interchange.positions[virtual_site_key.orientation_atom_indices[0]]
     r1 = interchange.positions[virtual_site_key.orientation_atom_indices[1]]
     r2 = interchange.positions[virtual_site_key.orientation_atom_indices[2]]
@@ -151,7 +192,7 @@ def _get_divalent_lone_pair_virtual_site_positions(
     rmid = (r1 + r2) / 2
     rmid_distance = numpy.sqrt(numpy.sum((r0 - rmid) ** 2))
 
-    if abs(r0_r1_bond_length - r0_r2_bond_length) > unit.Quantity(1e-3, unit.nanometer):
+    if abs(r0_r1_bond_length - r0_r2_bond_length) > Quantity(1e-3, unit.nanometer):
         raise VirtualSiteTypeNotImplementedError(
             "Only symmetric geometries (i.e. r2 - r0 = r1 - r0) are currently supported",
         )
