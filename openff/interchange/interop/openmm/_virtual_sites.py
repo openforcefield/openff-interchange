c8cc4b8c88467bdd85ebc15d0e7390ec5f874ab4
Matthew W. Thompson <mattwthompson@protonmail.com>
2023-10-12 09:08:08 -0500

    REF: Use `OutOfPlaneSite` for five-site water

--- openff/interchange/interop/openmm/_virtual_sites.py
+++ openff/interchange/interop/openmm/_virtual_sites.py
@@ -64,15 +64,17 @@ def _create_openmm_virtual_site(
         )
 
         distance = virtual_site.distance
+        out_of_plane_angle = virtual_site.out_of_plane_angle
 
         # TODO: Test r12 != r13, prima facia the math also applies, probably need
         #       a more direct way to get r1mid
-        if r12 == r13 and float(virtual_site.out_of_plane_angle.m) == 0.0:
+        if r12 == r13:
             r23 = _get_separation_by_atom_indices(
                 interchange=interchange,
-                atom_indices=virtual_site.orientations[1:],
+                atom_indices=virtual_site.orientations[1:3],
             )
 
+            # Angle formed by atoms 3, 1, 2
             theta = Quantity(
                 numpy.arccos(
                     (r23**2 - r12**2 - r13**2) / (-2 * r12 * r13),
@@ -80,19 +82,51 @@ def _create_openmm_virtual_site(
                 unit.radian,
             )
 
-            r1mid = Quantity(
-                numpy.cos(theta.m_as(unit.radian) / 2) * r12.m_as(unit.nanometer),
-                unit.nanometer,
-            )
+            if float(out_of_plane_angle.m) == 0.0:
+                r1mid = Quantity(
+                    numpy.cos(theta.m_as(unit.radian) / 2) * r12.m_as(unit.nanometer),
+                    unit.nanometer,
+                )
 
-            w1 = 1 + distance / r1mid
+                w1 = 1 + distance / r1mid
 
-            return openmm.ThreeParticleAverageSite(
-                *openmm_indices,
-                w1,
-                (1 - w1) / 2,
-                (1 - w1) / 2,
-            )
+                return openmm.ThreeParticleAverageSite(
+                    *openmm_indices,
+                    w1,
+                    (1 - w1) / 2,
+                    (1 - w1) / 2,
+                )
+
+            else:
+                # Special case out 5-site water, assumes symmetric geometry. Other cases
+                # fall back to LocalCoordinatesSite implementation
+                if sorted(
+                    [
+                        interchange.topology.atom(index).atomic_number
+                        for index in virtual_site.orientations
+                    ],
+                ) == [1, 1, 8]:
+                    distance_in_plane = distance * numpy.cos(
+                        out_of_plane_angle.to(unit.radian),
+                    )
+
+                    r1mid = r12 * numpy.cos(theta.m_as(unit.radian) / 2)
+
+                    w12 = w13 = -1 * distance_in_plane / r1mid / 2
+
+                    # units of inverse distance
+                    wcross = distance * numpy.sin(out_of_plane_angle.m_as(unit.radian))
+                    wcross /= r12 * r13 * numpy.sin(theta.m_as(unit.radian))
+
+                    if virtual_site.orientations[2] > virtual_site.orientations[1]:
+                        wcross *= -1
+
+                    return openmm.OutOfPlaneSite(
+                        *openmm_indices,
+                        w12,
+                        w13,
+                        wcross.m_as(1 / unit.nanometer),
+                    )
 
     # It is assumed that the first "orientation" atom is the "parent" atom.
     originwt, xdir, ydir = virtual_site.local_frame_weights
