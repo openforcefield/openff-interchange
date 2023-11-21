9e00eddd97671f2af19e793a13a7bea13cc27900
Matthew W. Thompson <mattwthompson@protonmail.com>
2023-11-21 13:53:04 -0600

    ENH: Support LJPME in LAMMPS export

--- openff/interchange/components/mdconfig.py
+++ openff/interchange/components/mdconfig.py
@@ -257,14 +257,19 @@ class MDConfig(DefaultModel):
             vdw_cutoff = round(self.vdw_cutoff.m_as(unit.angstrom), 4)
             coul_cutoff = round(self.coul_cutoff.m_as(unit.angstrom), 4)
 
-            if self.coul_method == _PME:
-                lmp.write(f"pair_style lj/cut/coul/long {vdw_cutoff} {coul_cutoff}\n")
-            elif self.coul_method == "cutoff":
-                lmp.write(f"pair_style lj/cut/coul/cut {vdw_cutoff} {coul_cutoff}\n")
+            if self.vdw_method == "pme" and self.coul_method == "pme":
+                lmp.write("pair_style lj/long/coul/long")
+            elif self.vdw_method == "cutoff":
+                if self.coul_method == _PME:
+                    lmp.write("pair_style lj/cut/coul/long")
+                elif self.coul_method == "cutoff":
+                    lmp.write("pair_style lj/cut/coul/cut")
             else:
                 raise UnsupportedExportError(
-                    f"Unsupported electrostatics method {self.coul_method}",
+                    "Unsupported nonbonded methods found: "
+                    f"{self.coul_method=}, {self.vdw_method=}",
                 )
+            lmp.write(f" {vdw_cutoff} {coul_cutoff}\n")
 
             if self.mixing_rule == "lorentz-berthelot":
                 lmp.write("pair_modify mix arithmetic tail yes\n\n")
