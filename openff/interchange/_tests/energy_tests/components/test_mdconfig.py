33c14038dfd0be7e51b4e9f5570da1b6c7d2fd62
Matthew W. Thompson <mattwthompson@protonmail.com>
2023-11-21 19:45:15 -0600

    BUG: Process long-range LJPME energy from GROMACS

--- openff/interchange/_tests/energy_tests/components/test_mdconfig.py
+++ openff/interchange/_tests/energy_tests/components/test_mdconfig.py
@@ -11,12 +11,12 @@ class TestLJPME:
         sage["vdW"].periodic_method = "Ewald3D"
 
         topology = MoleculeWithConformer.from_smiles("C#N").to_topology()
-        topology.box_vectors = [3, 3, 3] * unit.nanometer
+        topology.box_vectors = [2, 2, 2] * unit.nanometer
         interchange = sage.create_interchange(topology)
 
         get_openmm_energies(interchange, combine_nonbonded_forces=True).compare(
             get_gromacs_energies(interchange),
             tolerances={
-                "Nonbonded": Quantity(-51.9520537, "kilojoule / mole"),
+                "Nonbonded": Quantity(0.001, "kilojoule / mole"),
             },
         )
