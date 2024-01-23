88bf53ad491a7da1754064a857d6957d398f413f
Matthew W. Thompson <mattwthompson@protonmail.com>
2024-01-23 10:25:55 -0600

    TST: Test energy reporting with plugins

--- openff/interchange/_tests/unit_tests/drivers/test_openmm.py
+++ openff/interchange/_tests/unit_tests/drivers/test_openmm.py
@@ -1,15 +1,18 @@
 import pytest
-from openff.utilities.utilities import has_package, requires_package
+from openff.toolkit import ForceField
+from openff.utilities.utilities import has_package
 
+from openff.interchange._tests import MoleculeWithConformer, get_test_file_path
 from openff.interchange.constants import kj_mol
-from openff.interchange.drivers.openmm import _process
+from openff.interchange.drivers.openmm import _process, get_openmm_energies
 
 if has_package("openmm"):
     import openmm
 
 
-@requires_package("openmm")
 class TestProcess:
+    pytest.importorskip("openmm")
+
     @pytest.fixture()
     def dummy_system(self):
         system = openmm.System()
@@ -73,3 +76,44 @@ class TestProcess:
         assert processed["Electrostatics 1-4"].m_as(kj_mol) == -3
         assert processed["vdW"].m_as(kj_mol) == -1
         assert processed["vdW 1-4"].m_as(kj_mol) == -2
+
+
+class TestReportWithPlugins:
+    pytest.importorskip("smirnoff_plugins")
+
+    @pytest.fixture()
+    def ligand(self):
+        return MoleculeWithConformer.from_smiles("CC[C@@](/C=C\\[H])(C=C)O")
+
+    @pytest.fixture()
+    def de_force_field(self) -> ForceField:
+        return ForceField(
+            get_test_file_path("de-force-1.0.1.offxml"),
+            load_plugins=True,
+        )
+
+    @pytest.mark.parametrize("detailed", [True, False])
+    def test_nonzero_vdw(self, ligand, de_force_field, detailed):
+        energies = get_openmm_energies(
+            de_force_field.create_interchange(ligand.to_topology()),
+            combine_nonbonded_forces=False,
+            detailed=detailed,
+        )
+
+        assert energies["vdW"].m != 0
+
+        if detailed:
+            assert energies["vdW 1-4"].m != 0
+
+    def test_detailed_same_total_energy(self, ligand, de_force_field):
+        assert get_openmm_energies(
+            de_force_field.create_interchange(ligand.to_topology()),
+            combine_nonbonded_forces=False,
+            detailed=True,
+        ).total_energy.m == pytest.approx(
+            get_openmm_energies(
+                de_force_field.create_interchange(ligand.to_topology()),
+                combine_nonbonded_forces=False,
+                detailed=False,
+            ).total_energy.m,
+        )
