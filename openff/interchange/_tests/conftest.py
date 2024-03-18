8114412ea684e4a64cb5eca74e38b1023fddf54b
Matthew W. Thompson <mattwthompson@protonmail.com>
2024-03-18 13:43:46 -0500

    TST: Reproduce issue #935

--- openff/interchange/_tests/conftest.py
+++ openff/interchange/_tests/conftest.py
@@ -572,3 +572,11 @@ def no_charges() -> ForceField:
     )
 
     return sage
+
+
+@pytest.fixture()
+def popc():
+    return MoleculeWithConformer.from_smiles(
+        "CCCCCCCCCCCCCCCC(=O)OCC(COP(=O)([O-])OCC[N+](C)(C)C)OC(=O)CCCCCCCC=CCCCCCCCC",
+        allow_undefined_stereo=True,
+    )
