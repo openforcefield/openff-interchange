c98e14ebf93106054460c88a45e02efab562426c
pbuslaev <pbuslaev@gmail.com>
2024-04-15 14:12:39 +0100

    Update openff/interchange/interop/gromacs/export/_export.py
    Co-authored-by: Matt Thompson <matt.thompson@openforcefield.org>

--- openff/interchange/interop/gromacs/export/_export.py
+++ openff/interchange/interop/gromacs/export/_export.py
@@ -32,7 +32,7 @@ class GROMACSWriter(DefaultModel):
 
         with open(self.top_file, "w") as top:
             self._write_defaults(top)
-            to_reduced_atom_types = self._write_atomtypes(top)
+            reduced_atom_types = self._write_atomtypes(top)
 
             self._write_moleculetypes(top, to_reduced_atom_types)
 
