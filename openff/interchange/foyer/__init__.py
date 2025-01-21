c30a5b4f0f34da87dc1d046cf23f37a3e578afed
Matthew W. Thompson <mattwthompson@protonmail.com>
2025-01-21 10:52:23 -0600

    MAINT: Debug import

--- openff/interchange/foyer/__init__.py
+++ openff/interchange/foyer/__init__.py
@@ -3,7 +3,7 @@
 # openff/interchange/foyer.py causes "import foyer" to not crash, which breaks has_packge("foyer")
 # and breaks @skip_if_missing("foyer"), so need a more specific way of checking Foyer installation
 try:
-    from foyer.forcefields import __file__
+    from foyer import Forcefield
 
     has_foyer = True
 except (ModuleNotFoundError, AttributeError):
