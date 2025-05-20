8944e3908664d97346046fb44361d9be46e7580b
Matthew W. Thompson <mattwthompson@protonmail.com>
2025-05-20 10:39:10 -0500

    ENH: Treat asterisks and comments

--- openff/interchange/_tests/unit_tests/interop/gromacs/_import/test_from_files.py
+++ openff/interchange/_tests/unit_tests/interop/gromacs/_import/test_from_files.py
@@ -1,4 +1,8 @@
+import numpy
+import pytest
+
 from openff.interchange._tests import get_test_file_path
+from openff.interchange.exceptions import GROMACSParseError
 from openff.interchange.interop.gromacs._import._import import from_files
 from openff.interchange.interop.gromacs.models.models import RyckaertBellemansDihedral
 
@@ -15,3 +19,33 @@ def test_load_rb_torsions(monkeypatch):
 
     for torsion in system.molecule_types["Compound"].dihedrals:
         assert isinstance(torsion, RyckaertBellemansDihedral)
+
+
+def test_parse_bad_characters(monkeypatch):
+    monkeypatch.setenv("INTERCHANGE_EXPERIMENTAL", "1")
+
+    with pytest.raises(
+        GROMACSParseError,
+        match="garbage.*FLAG 8EE0CCA23E",
+    ):
+        # gro file doesn't need to have contents, crash should happen on the
+        # topology file which happens to be parsed first
+
+        from_files(
+            get_test_file_path("invalid.top"),
+            get_test_file_path("invalid.gro"),
+        )
+
+
+def test_asterisks_are_comments(monkeypatch):
+    monkeypatch.setenv("INTERCHANGE_EXPERIMENTAL", "1")
+
+    imported = from_files(
+        get_test_file_path("asterisk.top"),
+        get_test_file_path("asterisk.gro"),
+    )
+
+    # just check a few details, non-exhaustive
+    assert imported.vdw_14 == 0.5
+    assert imported.molecule_types["MOL0"].atoms[0].charge.m == -0.834
+    assert numpy.allclose(imported.box.m_as("nanometer"), 3 * numpy.eye(3))
