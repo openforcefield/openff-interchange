124b5e5e33ac4d09c59ada830224ad66ef2d0410
Matthew W. Thompson <mattwthompson@protonmail.com>
2025-07-08 14:48:06 -0500

    API: Keep old API in place with warning

--- openff/interchange/components/_packmol.py
+++ openff/interchange/components/_packmol.py
@@ -2,6 +2,24 @@
 A wrapper around PACKMOL. Adapted from OpenFF Evaluator v0.4.3.
 """
 
+from openff.interchange.packmol import (
+    RHOMBIC_DODECAHEDRON,
+    RHOMBIC_DODECAHEDRON_XYHEX,
+    UNIT_CUBE,
+    pack_box,
+    solvate_topology,
+    solvate_topology_nonwater,
+)
+
+__all__ = (
+    "RHOMBIC_DODECAHEDRON",
+    "RHOMBIC_DODECAHEDRON_XYHEX",
+    "UNIT_CUBE",
+    "pack_box",
+    "solvate_topology",
+    "solvate_topology_nonwater",
+)
+
 import warnings
 
 warnings.warn(
