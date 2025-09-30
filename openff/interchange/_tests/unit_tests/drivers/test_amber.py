2fa723239b1cba5fc5029cd64b8865404adca46d
Matthew W. Thompson <mattwthompson@protonmail.com>
2025-09-30 09:24:42 -0500

    FIX: Fix regex in test

--- openff/interchange/_tests/unit_tests/drivers/test_amber.py
+++ openff/interchange/_tests/unit_tests/drivers/test_amber.py
@@ -5,6 +5,7 @@ from openff.interchange.drivers.amber import AmberError, get_amber_energies
 
 
 def test_error_bad_vdw():
+    """Force Amber to report bad vdW energies by putting two atoms too close together."""
     x = ForceField("openff-2.0.0.offxml").create_interchange(
         Topology.from_molecules(
             [
@@ -24,8 +25,10 @@ def test_error_bad_vdw():
         "angstrom",
     )
 
+    # Resulting error message to match is:
+    # Found bad energy value ' *************' associated with energy type 'VDWAALS'
     with pytest.raises(
         AmberError,
-        match=r"Found bad energy value ' *************' associated with energy type 'VDWAALS'",
+        match=r"Found bad energy value.* \*\*\*\*\*\*\*\*\*\*\*\*\*. associated with energy type .VDWAALS.",
     ):
         get_amber_energies(x)
