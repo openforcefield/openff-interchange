46bc87205c5af6263b163f99e4447ef7291fe02d
Matthew W. Thompson <mattwthompson@protonmail.com>
2026-01-21 17:59:43 -0600

    BUG: Map `SingleAtomChargeTopologyKey` to `TopologyKey` just before OpenMM export

--- openff/interchange/common/_nonbonded.py
+++ openff/interchange/common/_nonbonded.py
@@ -1,4 +1,5 @@
 import abc
+from openff.interchange.models import TopologyKey, SingleAtomChargeTopologyKey
 from collections.abc import Iterable
 from typing import Any, Literal
 
@@ -8,8 +9,8 @@ from pydantic import Field, PrivateAttr, computed_field
 from openff.interchange._annotations import _DistanceQuantity, _ElementaryChargeQuantity
 from openff.interchange.components.potentials import Collection
 from openff.interchange.models import (
+    BaseVirtualSiteKey,
     LibraryChargeTopologyKey,
-    TopologyKey,
     VirtualSiteKey,
 )
 
@@ -146,3 +147,24 @@ class ElectrostaticsCollection(_NonbondedCollection):
             topology_key: self.potentials[potential_key].parameters["charge"]
             for topology_key, potential_key in self.key_map.items()
         }
+
+
+def _simpler_charges(
+    collection: ElectrostaticsCollection,
+) -> dict[TopologyKey | BaseVirtualSiteKey, Quantity]:
+    """Unify SingleAtomChargeTopologyKey to TopologyKey."""
+    return_dict = dict()
+
+    for key, val in collection.charges.items():
+        if isinstance(key, SingleAtomChargeTopologyKey):
+            new_key = TopologyKey(atom_indices=key.atom_indices)
+            return_dict[new_key] = val
+        elif isinstance(key, TopologyKey):
+            return_dict[key] = val
+        elif isinstance(key, BaseVirtualSiteKey):
+            return_dict[key] = val
+
+        else:
+            raise KeyError(f"Unexpected key of type {type(key)=}")
+
+    return return_dict
--- openff/interchange/interop/openmm/_nonbonded.py
+++ openff/interchange/interop/openmm/_nonbonded.py
@@ -12,7 +12,7 @@ from openff.units.openmm import to_openmm as to_openmm_quantity
 from openff.utilities.utilities import has_package
 
 from openff.interchange import Interchange
-from openff.interchange.common._nonbonded import ElectrostaticsCollection, vdWCollection
+from openff.interchange.common._nonbonded import ElectrostaticsCollection, vdWCollection, _simpler_charges
 from openff.interchange.constants import _PME
 from openff.interchange.exceptions import (
     CannotSetSwitchingFunctionError,
@@ -368,7 +368,7 @@ def _create_single_nonbonded_force(
             )
 
     if data.electrostatics_collection is not None:
-        partial_charges = data.electrostatics_collection.charges
+        partial_charges = _simpler_charges(data.electrostatics_collection)
 
     # mapping between (openmm) index of each atom and the (openmm) index of each virtual particle
     #   of that parent atom (if any)
@@ -959,7 +959,7 @@ def _set_particle_parameters(
     #       handling for electrostatics_force = None
     electrostatics: ElectrostaticsCollection = data.electrostatics_collection
 
-    partial_charges = electrostatics.charges
+    partial_charges = _simpler_charges(electrostatics)
 
     vdw: vdWCollection = data.vdw_collection
 
