4ab1276c5096a9d61b9181e4ffc1ec884771a6eb
Matthew W. Thompson <mattwthompson@protonmail.com>
2026-02-05 08:53:01 -0600

    FIX: Fix imports

--- openff/interchange/components/_packmol.py
+++ openff/interchange/components/_packmol.py
@@ -3,8 +3,7 @@ A wrapper around PACKMOL. Adapted from OpenFF Evaluator v0.4.3.
 """
 
 from openff.packmol import RHOMBIC_DODECAHEDRON, UNIT_CUBE
-from openff.packmol import (
-    RHOMBIC_DODECAHEDRON_XYHEX,
+from openff.packmol._packmol import (
     _find_packmol,
     _check_add_positive_mass,
     _check_box_shape_shape,
@@ -23,9 +22,12 @@ from openff.packmol import (
     _get_packmol_version,
     _build_input_file,
     _center_topology_at,
-    pack_box,
     _max_dist_between_points,
     _load_positions,
+)
+from openff.packmol import (
+    RHOMBIC_DODECAHEDRON_XYHEX,
+    pack_box,
     solvate_topology,
     solvate_topology_nonwater,
 )
