7307550fc32f0956c86e7492a1ac43847c67a2ad
Matthew W. Thompson <mattwthompson@protonmail.com>
2026-02-27 08:35:54 -0600

    TST: Expand cache/regression tests to other engines

--- openff/interchange/_tests/test_issues.py
+++ openff/interchange/_tests/test_issues.py
@@ -10,7 +10,7 @@ from openff.utilities import get_data_file_path, has_executable, skip_if_missing
 from openff.interchange import Interchange
 from openff.interchange._tests import MoleculeWithConformer, needs_gmx, shuffle_topology
 from openff.interchange.components._packmol import UNIT_CUBE, pack_box, solvate_topology
-from openff.interchange.drivers import get_amber_energies, get_openmm_energies
+from openff.interchange.drivers import get_amber_energies, get_gromacs_energies, get_openmm_energies
 from openff.interchange.exceptions import NonperiodicNoCutoffNotSupportedError
 
 
@@ -262,39 +262,84 @@ def test_issue_1433_openmm(sage, valence_handler):
     assert abs(original_energy - new_energy) > 0.1
 
 
-def test_isssue_1450_reassign_vdw(sage, water_dimer):
+@pytest.mark.parametrize("engine", ["openmm", "amber", "gromacs"])
+def test_issue_1450_reassign_vdw(sage, water_dimer, engine):
     """Sanity check that re-assigning vdW parameters actually changes energies."""
+
+    if not has_executable("sander"):
+        pytest.skip(reason="sander not installed")
+
+    pytest.importorskip("openmm")
+
     original_interchange = sage.create_interchange(water_dimer)
 
-    original_energy = get_openmm_energies(original_interchange).energies["Nonbonded"]
+    match engine:
+        case "openmm":
+            original_energy = get_openmm_energies(original_interchange, combine_nonbonded_forces=False).energies["vdW"]
+        case "amber":
+            original_energy = get_amber_energies(original_interchange).energies["vdW"]
+        case "gromacs":
+            original_energy = get_gromacs_energies(original_interchange).energies["vdW"]
 
     for potential in original_interchange["vdW"].potentials.values():
         potential.parameters["epsilon"] = potential.parameters["epsilon"] * 100
 
-    new_energy = get_openmm_energies(original_interchange).energies["Nonbonded"]
+    match engine:
+        case "openmm":
+            new_energy = get_openmm_energies(original_interchange, combine_nonbonded_forces=False).energies["vdW"]
+        case "amber":
+            new_energy = get_amber_energies(original_interchange).energies["vdW"]
+        case "gromacs":
+            new_energy = get_gromacs_energies(original_interchange).energies["vdW"]
 
     assert original_energy != new_energy, "Energies should be different after modifying vdW parameters"
 
 
+@pytest.mark.parametrize("engine", ["openmm", "amber", "gromacs"])
 @pytest.mark.parametrize("valence_handler", ["Bonds", "Angles", "ProperTorsions"])
-def test_issue_1450_reassign_valence(sage, valence_handler):
+def test_issue_1450_reassign_valence(sage, valence_handler, engine):
     """Sanity check that re-assigning valence parameters actually changes energies."""
+
     topology = MoleculeWithConformer.from_smiles(
-        "c1c(cccc1)C(c2ccccc2)c3ccccc3",
+        "CC(=O)NC",
         allow_undefined_stereo=True,
     ).to_topology()
+
+    topology.box_vectors = Quantity(numpy.eye(3) * 10.0, "nanometer")
+
     original_interchange = sage.create_interchange(topology)
 
-    original_energy = get_openmm_energies(original_interchange)
+    match valence_handler:
+        case "Bonds":
+            shorthand = "Bond"
+        case "Angles":
+            shorthand = "Angle"
+        case "ProperTorsions":
+            shorthand = "Torsion"
+
+    match engine:
+        case "openmm":
+            original_energy = get_openmm_energies(original_interchange).energies[shorthand]
+        case "amber":
+            original_energy = get_amber_energies(original_interchange).energies[shorthand]
+        case "gromacs":
+            original_energy = get_gromacs_energies(original_interchange).energies[shorthand]
 
     for potential in original_interchange[valence_handler].potentials.values():
         potential.parameters["k"] = potential.parameters["k"] * 100
 
-    new_energy = get_openmm_energies(original_interchange)
+    match engine:
+        case "openmm":
+            new_energy = get_openmm_energies(original_interchange).energies[shorthand]
+        case "amber":
+            new_energy = get_amber_energies(original_interchange).energies[shorthand]
+        case "gromacs":
+            new_energy = get_gromacs_energies(original_interchange).energies[shorthand]
 
     assert original_energy != new_energy, "Energies should be different after modifying valence parameters"
 
 
+@skip_if_missing("openmm")
 @pytest.mark.parametrize("valence_handler", ["Bonds", "Angles", "ProperTorsions", "ImproperTorsions"])
 def test_issue_1234_openmm(sage, valence_handler):
     """Test that modifications to a `ForceField` object are reflected in re-creating new `Interchange`s."""
