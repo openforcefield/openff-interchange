d68088a438464f1ace0577cb52068bfecbe68f81
Matthew W. Thompson <mattwthompson@protonmail.com>
2026-04-03 17:24:41 -0500

    TST: Actually test re-export

---
+++ openff/interchange/_tests/unit_tests/components/test_packmol.py
@@ -0,0 +1,29 @@
+import shutil
+
+import pytest
+from openff.utilities import has_executable, has_package
+
+
+@pytest.mark.skipif(
+    not has_executable("packmol"),
+    reason="Packmol is not available on this system",
+)
+def test_basic_reexport():
+    """Test that the packmol module can be imported."""
+    from openff.interchange.components._packmol import _find_packmol, pack_box
+
+    assert _find_packmol() == shutil.which("packmol")
+
+    assert "interchange" not in pack_box.__module__
+
+
+@pytest.mark.skipif(
+    not has_package("openff.packmol"),
+)
+def test_import_error_when_openff_packmol_not_installed():
+    with pytest.raises(
+        ImportError,
+    ):
+        from openff.interchange.components._packmol import pack_box
+
+        assert "interchange" not in pack_box.__module__
